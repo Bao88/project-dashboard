0a0d9a1bc4b094ddb1234c07aa90bca896a60f98
bao88 <bao88@live.no>
2022-05-17 17:46:41 +0200

    - connect using MONGO_URL when deployed

--- src/lib/database/setup.ts
+++ src/lib/database/setup.ts
@@ -24,12 +24,14 @@ class Setup {
      * @returns string - URL to connect to MongoDB database
      */
     private getDatabaseURL(){
-        const prefix = 'mongodb://'
-        const authentication = `${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}`
-        const internalName = `@${process.env.MONGO_INITDB_INTERNAL_NAME}:${process.env.MONGO_INITDB_PORT}/`
-        const database = process.env.MONGO_INITDB_DATABASE
-
-        return `${prefix}${authentication}${internalName}${database}`
+        if(process.env.NODE_ENV === 'localhost') {
+            const prefix = 'mongodb://'
+            const authentication = `${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}`
+            const internalName = `@${process.env.MONGO_INITDB_INTERNAL_NAME}:${process.env.MONGO_INITDB_PORT}/`
+            const database = process.env.MONGO_INITDB_DATABASE
+            return `${prefix}${authentication}${internalName}${database}`
+        } else return process.env.MONGO_URL
+       
     }
 }
 const setup = new Setup()
